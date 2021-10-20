c23b4024647aa76b3cfbb1b849e544a84e9de8d2
kikpond15 <kikpond15@gmail.com>
2021-10-20 19:43:54 +0900

    update all

--- FrameFormatter.js
+++ FrameFormatter.js
@@ -4,98 +4,99 @@ let inputTitle;
 let inputImage;
 let inputURL;
 
-let img, previewImg;
 let previewButton;
 let saveButton;
 let pg;
-let tagDiv;
-
-//A4 pix size 2894 x 4093
+let p5Img, previewImg;
+let qrImage;
+let qrcode;
+let qrDiv;
+//A4 pix size 2894 x 
 
 function preload() {
-  myFont = loadFont('assets/Inconsolata-Regular.ttf');
+    myFont = loadFont('assets/Inconsolata-Regular.ttf');
 }
 
 function setup() {
-  createCanvas(windowWidth, 800);
-  textFont(myFont);
-  textSize(20);
+    createCanvas(2894, 3507);
+    textFont(myFont);
+    textSize(20);
 
-  inputName = createInput();
-  inputName.position(20, 60);
+    inputName = createInput();
+    inputName.position(20, 60);
 
-  inputTitle = createInput();
-  inputTitle.position(20, 120);
+    inputTitle = createInput();
+    inputTitle.position(20, 120);
 
-  inputImage = createFileInput(handleFile);
-  inputImage.position(20, 175);
+    inputImage = createFileInput(handleFile);
+    inputImage.position(20, 175);
 
-  inputURL = createInput('');
-  inputURL.position(20, 235);
+    inputURL = createInput('');
+    inputURL.position(20, 235);
 
-  previewButton = createButton('preview');
-  previewButton.position(20, 270);
-  previewButton.mousePressed(preview);
+    previewButton = createButton('preview');
+    previewButton.position(20, 270);
+    previewButton.mousePressed(preview);
 
-  saveButton = createButton('export');
-  saveButton.position(100, 270);
-  saveButton.mousePressed(exportImage);
+    saveButton = createButton('export');
+    saveButton.position(100, 270);
+    saveButton.mousePressed(exportImage);
 
-  imageMode(LEFT);
+    imageMode(LEFT);
+
+    qrDiv = createDiv('');
 }
 
 function draw() {
-  background(180);
-  text('creater name', 20, 50);
-  text('title', 20, 110);
-  text('inport image', 20, 170);
-  text('code site url(openProcessing/p5 editor/GitHub)', 20, 230);
+    background(180);
+    text('creater name', 20, 50);
+    text('title', 20, 110);
+    text('inport image', 20, 170);
+    text('code site url(openProcessing/p5 editor/GitHub)', 20, 230);
 
-  if (previewImg) {
-    image(previewImg, 20, 300, 289, 409);
-  }
+    if (previewImg) {
+        image(previewImg, 20, 300, 289, 409);
+    }
+    if (qrImage) {
+        image(qrImage, 20, 300, 289, 409);
+    }
 }
 
 function preview() {
-  let name = inputName.value();
-  let title = inputTitle.value();
-  if (img) {
-    pg = createGraphics(2480, 3507); //A4 size(2894 x 4093)
-    pg.background(255);
-    pg.imageMode(CENTER);
-    pg.image(img, pg.width/2, pg.height/2); //center position
-    stroke(0);
-    pg.textSize(100);
-    pg.textFont(myFont);
-    pg.text(title, 150, 3200);
-    pg.text(name, 150, 3360);
+    let name = inputName.value();
+    let title = inputTitle.value();
 
-    if (inputURL != null) {
-      let qr = qrcode(0, 'L');
-      qr.addData(inputURL.value());
-      qr.make();
-      let qrImg = qr.createImgTag(5, 20, "qr code");
-      tagDiv = createDiv();
-      tagDiv.position(350, 300);
-      tagDiv.html(qrImg);
-      
-      //pg.image(, 2500,3200);
+    // if (inputURL != null) {
+    //     //let qrcode = new QRcode();
+    //     qrcode.makeCode(inputURL.value());
+    //     console.log(qr);
+    // }
+
+    if (p5Img) {
+        pg = createGraphics(2480, 3507); //A4 size(2894 x 3507)
+        pg.background(255);
+        pg.imageMode(CENTER);
+        pg.image(p5Img, pg.width / 2, pg.height / 2); //center position
+        stroke(0);
+        pg.textSize(100);
+        pg.textFont(myFont);
+        pg.text(title, 150, 3200);
+        pg.text(name, 150, 3360);
+
+        previewImg = pg;
     }
-
-    previewImg = pg;
-  }
 }
 
 function handleFile(file) {
-  print(file);
-  if (file.type === 'image') {
-    img = createImg(file.data, '');
-    img.hide();
-  } else {
-    img = null;
-  }
+    print(file);
+    if (file.type === 'image') {
+        p5Img = createImg(file.data, '');
+        p5Img.hide();
+    } else {
+        p5Img = null;
+    }
 }
 
 function exportImage() {
-  save(pg, 'myCanvas.jpg');
+    save('myCanvas', 'svg');
 }
---
+++ qr_code_2021_10_20_06_05_10/sketch.js
@@ -0,0 +1,61 @@
+let qrcode;
+let div;
+let img;
+
+function setup() {
+    createCanvas(800, 800);
+
+
+    div = createDiv("");
+    div.id("qrcode");
+
+    div.style("width", "256px");
+    div.style("height", "256px");
+    div.style("padding", "2px");
+    div.style("background-color", "grey");
+    div.position(10, 10);
+
+    qrcode = new QRCode("qrcode");
+
+}
+
+function draw() {
+    //  background(255,0,0);
+    if (img) {
+        image(img, 0, 300, 256, 256);
+    }
+}
+
+function makeCode() {
+    let url = "www.naver.com";
+    qrcode.makeCode(url);
+
+    const qrCodeContainer = document.getElementById('qrcode');
+    const qrChildren = qrCodeContainer.children;
+    const qrCodeImage = qrChildren[1];
+    const base64 = qrCodeImage.getAttribute('src');
+
+    console.log(qrChildren);
+    console.log(qrCodeImage);
+    console.log(typeof base64);
+    //img = loadImage(base64);
+
+}
+
+
+function keyPressed() {
+
+    if (key == '0') {
+
+        div.remove();
+
+        div = createDiv("");
+        div.id("qrcode");
+
+        div.position(0, 0);
+
+        qrcode = new QRCode("qrcode");
+    } else if (key == '1') {
+        makeCode();
+    }
+}
